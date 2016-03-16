609597199fbf1a6eb98148c8fa9ca4efb397e546
Giovani Gonzalez <darkgeat@gmail.com>
2016-03-16 09:26:52 -0600

    Preparation for the Initial Steps, Creation of Video Edition activity and structure, added CameraRecording screen and all the resources for CameraRecording. Also Cleaning Initial code of Main Screen

---
+++ app/src/main/java/com/itexico/videotrimming/activities/VideoEdition.java
@@ -0,0 +1,18 @@
+package com.itexico.videotrimming.activities;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.itexico.videotrimming.R;
+
+/**
+ * Created by darkgeat on 3/16/16.
+ */
+public class VideoEdition extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_video_edition);
+    }
+}
